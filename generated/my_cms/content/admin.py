bd07dcaafdae869cf982c00a36af1e7512f2db0b
JeremyRos08 <jeremyros08@gmail.com>
2026-08-17 03:44:02 +0200

    chore: remove generated CMS build from tracking

--- generated/my_cms/content/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-@admin.register(models.Vehicule)
-class VehiculeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'marque', 'modele', 'immatriculation', 'updated_at')
-    search_fields = ('marque', 'modele', 'immatriculation')
--- generated/my_cms/content/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class VehiculeForm(forms.ModelForm):
-    class Meta:
-        model = models.Vehicule
-        fields = '__all__'
